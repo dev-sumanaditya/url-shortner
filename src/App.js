565de272c7eac0d9b1d47f0be8cbc53af8162eb3
Aditya Suman <sumanaditya19@gmail.com>
2022-08-18 23:33:07 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [value, setValue] = useState('');
   const [error, setError] = useState('');
 
+  const [show, setShow] = useState(false);
+
   function isValidHttpUrl(string) {
     let url;
     
@@ -21,7 +23,8 @@ function App() {
     console.log('opening url in safari')
     if(isValidHttpUrl(value)) {
       setError('');
-      window.open('safari-'+value)
+      const url = 'safari-' + value
+      window.open(url)
     } else {
       setError('Enter a valid URL.');
     }
@@ -30,12 +33,15 @@ function App() {
 
 
   return (
-    <div className="bg-teal-100 h-screen p-4">
-        <div className="h-full w-full flex flex-col gap-8 items-center justify-center">
+    <div className="bg-teal-100 min-h-screen p-4">
+        <div className="pt-14 flex flex-col gap-6 items-center justify-center">
           <h1 className='text-2xl text-center'>Enter Url to open in Safari</h1>
           <input className='px-6 py-3 text-xl border-2 rounded border-black/80 w-full md:w-1/3 ' value={value} onChange={e => setValue(e.target.value)} type="text" placeholder="Enter url" />
           <p className='text-red-500 font-bold text-lg'>{error}</p>
           <button onClick={openURL} className='px-10 py-3 bg-black hover:bg-black/80 duration-200 text-white uppercase rounded'>Open</button>
+          <a className='text-xl font-bold' href="safari-https://deltacodes.in">link 1.</a>
+          <a className='text-xl font-bold' href="safari-http://deltacodes.in">link 2.</a>
+          <a className='text-xl font-bold' href="safari-https://deltacodes.in">link 3.</a>
         </div>
     </div>
   );
